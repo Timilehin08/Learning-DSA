fae97a2933c7025e1cd0aea10e08a4a06c418aa0
Timilehin08 <52857292+Timilehin08@users.noreply.github.com>
2021-12-14 23:36:13 +0100

    syntax

--- binarysearch/syntax/syn.py
+++ binarysearch/syntax/syn.py
@@ -16,3 +16,44 @@ list = ["Lists can have", 1, 2, 3, 4, "or more types together!"]
 tuple = ("Tuples", "can have", "more than", 2, "elements!")
 dictionary = {'one': 1, 'two': 2, 'three': 3}
 variable_with_zero_data = None
+
+Simple Logging
+print "Printed!"
+
+
+Conditionals
+if cake == "delicious":
+    return "Yes please!"
+elif cake == "okay":
+    return "I'll have a small piece."
+else:
+    return "No, thank you."
+
+		Loops
+		for item in list:
+    print item
+
+while (total < max_val):
+    total += values[i]
+    i += 2
+
+		Functions
+		def divide(dividend, divisor):
+    quotient = dividend / divisor
+    remainder = dividend % divisor
+    return quotient, remainder
+
+def calculate_stuff(x, y):
+    (q, r) = divide(x,y)
+    print q, r
+
+		Classes
+		class Person(object):
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age 
+
+    def birthday(self):
+        self.age += 1
+
+				
